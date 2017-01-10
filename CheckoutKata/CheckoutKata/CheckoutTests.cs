8ce9b5705e029936ff4905fc8eccf3c725eee27f
bob poole <bob.poole@brighthr.com>
2017-01-10 19:22:55 +0000

    First broken test

--- CheckoutKata/CheckoutKata/CheckoutTests.cs
+++ CheckoutKata/CheckoutKata/CheckoutTests.cs
@@ -4,10 +4,20 @@ namespace CheckoutKata
 {
     public class CheckoutTests
     {
-        [Test]
-        public void ScanItem()
+        private ICheckout _checkout;
+
+        [SetUp]
+        public void setup()
         {
-            
+            _checkout = new Checkout();       
+        }
+
+        [Test]
+        public void ScanItemAReturns50()
+        {
+            _checkout.ScanItem("A");
+
+            Assert.That(_checkout.GetTotalPrice(), Is.EqualTo(50));
         }
     }
 }
