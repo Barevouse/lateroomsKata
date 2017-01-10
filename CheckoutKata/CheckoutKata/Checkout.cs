b6e3670280cf09afab461d2b7125d884f9da1090
bob poole <bob.poole@brighthr.com>
2017-01-10 20:20:36 +0000

    Now includes B

--- CheckoutKata/CheckoutKata/Checkout.cs
+++ CheckoutKata/CheckoutKata/Checkout.cs
@@ -1,15 +1,25 @@
-﻿namespace CheckoutKata
+﻿using System.Collections.Generic;
+
+namespace CheckoutKata
 {
     public class Checkout : ICheckout
     {
+        public List<string> Codes = new List<string>();
         public void ScanItem(string SKU)
         {
-            
+            Codes.Add(SKU);
         }
 
         public int GetTotalPrice()
         {
-            return 50;
+            if (Codes[0] == "A")
+            {
+                return 50;
+            }
+            else
+            {
+                return 30;
+            }
         }
     }
 }
