dbf8f0e9b28d1bcaee95237df1765ce03034105d
bob poole <bob.poole@brighthr.com>
2017-01-10 19:03:53 +0000

    First Commit

---
+++ CheckoutKata/CheckoutKata/Checkout.cs
@@ -0,0 +1,15 @@
+﻿namespace CheckoutKata
+{
+    public class Checkout : ICheckout
+    {
+        public void ScanItem(string SKU)
+        {
+            
+        }
+
+        public int GetTotalPrice()
+        {
+            return 0;
+        }
+    }
+}
---
+++ CheckoutKata/CheckoutKata/CheckoutTests.cs
@@ -0,0 +1,13 @@
+﻿using NUnit.Framework;
+
+namespace CheckoutKata
+{
+    public class CheckoutTests
+    {
+        [Test]
+        public void ScanItem()
+        {
+            
+        }
+    }
+}
